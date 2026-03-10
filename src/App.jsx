c8c1fa05668c9c4fb8f7bec6901f3ebba7679212
ahmedtajdev <ahmedtaj.dev@gmail.com>
2026-03-10 18:45:24 +0200

    authcontext fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Register from "./Components/Register/Register";
 import Notfound from "./Components/Notfound/Notfound";
 import { HeroUIProvider } from "@heroui/react";
 import Profile from "./Components/Profile/Profile";
-import AuthContextProvider from "./Context/authContext";
+import AuthContextProvider from "./Context/AuthContext";
 import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute";
 import AntiProtectedRoute from "./Components/AntiProtectedRoute/AntiProtectedRoute";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
